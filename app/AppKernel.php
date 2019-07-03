f1bac17b906506491f1ae01c6aab18f8163d8aae
rhking95 <radhouane.rh@gmail.com>
2019-07-03 18:57:15 +0100

    Creation Bundle
    Creation des Bundle : Service, Produit, Evenement et Demande

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new ServiceBundle\ServiceBundle(),
+            new ProduitBundle\ProduitBundle(),
+            new EvenementBundle\EvenementBundle(),
+            new DemandeBundle\DemandeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
