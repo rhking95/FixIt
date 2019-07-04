51ae7c56df4605a22e7c3b34ab28d9d2e75ef0db
zodiacxy <yesser.somai@esprit.tn>
2019-07-04 10:55:13 +0100

    Commit initial

--- src/DemandeBundle/Controller/DefaultController.php
+++ src/DemandeBundle/Controller/DefaultController.php
@@ -6,9 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-
     public function indexAction()
     {
-        return $this->render('@Demande:Default:index.html.twig');
+        return $this->render('DemandeBundle:Default:index.html.twig');
     }
 }
