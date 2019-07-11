0fd4178efd46167664331a85bd0594f357bec281
zodiacxy <yesser.somai@esprit.tn>
2019-07-11 23:35:02 +0100

    commit

--- var/cache/dev/twig/27/27696215e31be22afdbd9532dcb7dcb102c5cf05ec04a06d456285f2181bb070.php
+++ var/cache/dev/twig/27/27696215e31be22afdbd9532dcb7dcb102c5cf05ec04a06d456285f2181bb070.php
@@ -7,10 +7,16 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
     {
         parent::__construct($env);
 
-        $this->parent = false;
-
+        // line 1
+        $this->parent = $this->loadTemplate("base.html.twig", "@Demande/Default/demande.html.twig", 1);
         $this->blocks = array(
+            'content' => array($this, 'block_content'),
         );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
     }
 
     protected function doDisplay(array $context, array $blocks = array())
@@ -21,30 +27,37 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Demande/Default/demande.html.twig"));
 
-        // line 1
-        echo "<head>
-    <link href=\"";
-        // line 2
-        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("template/css/bootstrap.min.css"), "html", null, true);
-        echo "\" rel=\"stylesheet\"/>
-    <link href=\"";
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 2
+    public function block_content($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
+
         // line 3
-        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("template/css/datepicker3.css"), "html", null, true);
-        echo "\" rel=\"stylesheet\"/>
-    <link href=\"";
-        // line 4
-        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("template/css/styles.css"), "html", null, true);
-        echo "\" rel=\"stylesheet\"/>
-</head>
-<body>
+        echo "<body>
 ";
-        // line 7
+        // line 4
         echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock(($context["form"] ?? $this->getContext($context, "form")), 'form_start');
         echo "
 
-<div class=\"col-lg-12\">
+<div class=\"col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main\">
     <div class=\"panel panel-default\">
-        <div class=\"panel-heading\">Ajouter votre demande</div>
+        <div class=\"panel-heading\">";
+        // line 8
+        echo twig_escape_filter($this->env, ($context["title"] ?? $this->getContext($context, "title")), "html", null, true);
+        echo "</div>
         <div class=\"panel-body\">
             <div class=\"form-group\">
 
@@ -56,15 +69,15 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
                 </tr>
                 <tr>
                     <td height=\"50px\">";
-        // line 22
+        // line 19
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "cat", array()), 'widget', array("attr" => array("class" => "form-control")));
         echo "</td>
                     <td>";
-        // line 23
+        // line 20
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "cat", array()), 'errors');
         echo "</td>
                     <td height=\"50px\">";
-        // line 24
+        // line 21
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "address", array()), 'widget', array("attr" => array("class" => "form-control", "placeholder" => "Address")));
         echo "</td>
 
@@ -76,15 +89,15 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
                 </tr>
                 <tr>
                     <td height=\"50px\">";
-        // line 33
+        // line 30
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "dis", array()), 'widget', array("attr" => array("class" => "form-control")));
         echo "</td>
                     <td>";
-        // line 34
+        // line 31
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "dis", array()), 'errors');
         echo "</td>
                     <td height=\"50px\">";
-        // line 35
+        // line 32
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "contact", array()), 'widget', array("attr" => array("class" => "form-control", "placeholder" => "Contact")));
         echo "</td>
                 </tr>
@@ -93,11 +106,11 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
                 </tr>
                 <tr>
                     <td height=\"30px\">";
-        // line 41
+        // line 38
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "photo", array()), 'widget', array("attr" => array("class" => "form-control", "placeholder" => "Photo")));
         echo "</td>
                     <td>";
-        // line 42
+        // line 39
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "photo", array()), 'errors');
         echo "</td>
                 </tr>
@@ -106,17 +119,17 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
                 </tr>
                 <tr>
                     <td height=\"30px\">";
-        // line 48
+        // line 45
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "prix", array()), 'widget', array("attr" => array("class" => "form-control", "placeholder" => "Prix")));
         echo "</td>
                     <td>";
-        // line 49
+        // line 46
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "prix", array()), 'errors');
         echo "</td>
                 </tr>
                 <tr>
                     <td height=\"50px\">";
-        // line 52
+        // line 49
         echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "Demander", array()), 'widget', array("attr" => array("class" => "btn btn-primary", "value" => "Valider")));
         echo "</td>
                 </tr>
@@ -125,18 +138,22 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
     </div>
     </div>
 </div>
+";
+        // line 56
+        echo $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock($this->getAttribute(($context["form"] ?? $this->getContext($context, "form")), "iduser", array()), 'widget', array("attr" => array("value" => $this->getAttribute($this->getAttribute(($context["app"] ?? $this->getContext($context, "app")), "user", array()), "id", array()))));
+        echo "
+";
+        // line 57
+        echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock(($context["form"] ?? $this->getContext($context, "form")), 'form_end');
+        echo "
 </body>
 
 ";
-        // line 61
-        echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock(($context["form"] ?? $this->getContext($context, "form")), 'form_end');
-        echo "
-</body>";
-        
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
-
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
 
     }
 
@@ -152,7 +169,7 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
 
     public function getDebugInfo()
     {
-        return array (  132 => 61,  120 => 52,  114 => 49,  110 => 48,  101 => 42,  97 => 41,  88 => 35,  84 => 34,  80 => 33,  68 => 24,  64 => 23,  60 => 22,  42 => 7,  36 => 4,  32 => 3,  28 => 2,  25 => 1,);
+        return array (  147 => 57,  143 => 56,  133 => 49,  127 => 46,  123 => 45,  114 => 39,  110 => 38,  101 => 32,  97 => 31,  93 => 30,  81 => 21,  77 => 20,  73 => 19,  59 => 8,  52 => 4,  49 => 3,  40 => 2,  11 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -165,17 +182,14 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
 
     public function getSourceContext()
     {
-        return new Twig_Source("<head>
-    <link href=\"{{ asset('template/css/bootstrap.min.css') }}\" rel=\"stylesheet\"/>
-    <link href=\"{{ asset('template/css/datepicker3.css') }}\" rel=\"stylesheet\"/>
-    <link href=\"{{ asset('template/css/styles.css') }}\" rel=\"stylesheet\"/>
-</head>
+        return new Twig_Source("{% extends 'base.html.twig' %}
+{% block content %}
 <body>
 {{ form_start(form)}}
 
-<div class=\"col-lg-12\">
+<div class=\"col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main\">
     <div class=\"panel panel-default\">
-        <div class=\"panel-heading\">Ajouter votre demande</div>
+        <div class=\"panel-heading\">{{ title }}</div>
         <div class=\"panel-body\">
             <div class=\"form-group\">
 
@@ -223,9 +237,10 @@ class __TwigTemplate_04083b851d93c014d4eeca3bd6f6bd70167e08a84c219ded4f9502f4d20
     </div>
     </div>
 </div>
+{{ form_widget(form.iduser,{ 'attr': {'value': app.user.id } }) }}
+{{ form_end(form)}}
 </body>
 
-{{ form_end(form)}}
-</body>", "@Demande/Default/demande.html.twig", "C:\\wamp64\\www\\FixIt\\src\\DemandeBundle\\Resources\\views\\Default\\demande.html.twig");
+{% endblock %}", "@Demande/Default/demande.html.twig", "C:\\wamp64\\www\\FixIt\\src\\DemandeBundle\\Resources\\views\\Default\\demande.html.twig");
     }
 }
